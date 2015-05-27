87fc1e5e693c4b4b22b51dcf523548a3988734e4
blabaere <benoit.labaere@gmail.com>
2015-05-27 21:54:14 +0200

    Finally got recursion and loop working using a boxed closure
    this means using f: &F instead of f: F

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,20 @@ fn evaluate<T>(values: &[T;9], constants: &[T;4]) -> T where
 	T : Copy {
 
 	values[0] + 
-	((constants[0] * values[1]) / values[2]) + 
+	(constants[0] * values[1] / values[2]) + 
 	values[3] + 
 	(constants[1] * values[4]) - 
 	values[5] - 
 	constants[2] + 
-	((values[6] * values[7]) / values[8]) - 
+	(values[6] * values[7] / values[8]) - 
 	constants[3]
 
 }
 
 const FLOAT_CONSTANTS: [f64;4]  = [13f64, 12f64, 11f64, 10f64];
-//const INT_CONSTANTS: [isize;4]  = [13, 12, 11, 10];
+const INT_CONSTANTS: [isize;4]  = [13, 12, 11, 10];
 
-fn swap<T : Copy>(values: &mut [T;9], i: usize, j: usize) {
+fn swap<T : Copy>(values: &mut [T], i: usize, j: usize) {
 	let temp = values[i];
 
 	values[i] = values[j];
@@ -53,6 +53,33 @@ fn permutation<T>(values: &mut [T;9], constants: &[T;4], from: usize, to: usize,
 	}
 }
 
+fn permute_and_check_slice<T, P>(values: &mut [T;9], from: usize, to: usize, predicate: &P) -> usize where 
+	T : Copy,
+	P : Fn(&[T;9]) -> bool {
+
+	if from == to {
+		if predicate(values) { 1 } else { 0 }
+	} else {
+		let mut count: usize = 0;
+		for j in from..to {
+
+			swap(values, from, j);
+			count += permute_and_check_slice(values, from + 1, to, predicate);
+			swap(values, from, j);
+		} 
+
+		count
+	}
+}
+
+fn permute_and_check<T, P>(values: &mut [T;9], predicate: &P) -> usize where 
+	T : Copy,
+	P : Fn(&[T;9]) -> bool {
+
+	let count = values.len();
+	permute_and_check_slice(values, 0, count, predicate)
+}
+
 fn evaluate_and_check<T>(values: &[T;9], constants: &[T;4], expected: T) -> bool where
 	T : Add<Output = T>,
 	T : Sub<Output = T>,
@@ -64,11 +91,37 @@ fn evaluate_and_check<T>(values: &[T;9], constants: &[T;4], expected: T) -> bool
 	evaluate(values, constants) == expected
 }
 
+fn find_int_solutions() -> usize {
+	let mut int_values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+	permute_and_check(&mut int_values, &|xs| { evaluate(xs, &INT_CONSTANTS) == 66 })
+}
+
+fn find_float_solutions() -> usize {
+	let mut int_values = [1f64, 2f64, 3f64, 4f64, 5f64, 6f64, 7f64, 8f64, 9f64];
+
+	permute_and_check(&mut int_values, &|xs| { evaluate(xs, &FLOAT_CONSTANTS) == 66f64 })
+}
+
+fn without_closure() {
+	let mut float_values = [1f64, 2f64, 3f64, 4f64, 5f64, 6f64, 7f64, 8f64, 9f64];
+	let mut int_values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	let mut result_count = permutation(&mut float_values, &FLOAT_CONSTANTS, 0, 9, 66f64);
+
+	println!("Float result count: {:?}", result_count);
+	result_count = permutation(&mut int_values, &INT_CONSTANTS, 0, 9, 66);
+	println!("Int result count: {:?}", result_count);
+}
+
+fn with_closure() {
+	let mut result_count = find_float_solutions();
+
+	println!("Float result count: {:?}", result_count);
+	result_count = find_int_solutions();
+	println!("Int result count: {:?}", result_count);
+}
 
 fn main() {
-	let mut float_values = [1f64, 2f64, 3f64, 4f64, 5f64, 6f64, 7f64, 8f64, 9f64];
-	//let mut int_values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-	let result_count = permutation(&mut float_values, &FLOAT_CONSTANTS, 0, 9, 66f64);
-
-	println!("Result count: {:?}", result_count);
+	without_closure();
+	with_closure();
 }
